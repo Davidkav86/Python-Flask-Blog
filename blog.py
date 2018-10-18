997e0b7329bc8dabad300455907e5eef1138ce5c
David <david_kav@hotmail.com>
2018-10-18 19:16:20 +0200

    db created - controller improved

--- blog.py
+++ blog.py
@@ -1,6 +1,17 @@
-from flask import Flask
+# CONTROLLER
+
+from flask import Flask, render_template, request, session, flash, redirect, url_for, g
+import sqlite3
 
 app = Flask(__name__)
 
+DATABASE = 'blog.db'
+
+# pulls in app configuration by looking for UPPERCASE variables
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
 if __name__ == "__main__":
-    app.run()
+    app.run(DEBUG=True)
---
+++ sql.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+with sqlite3.connect("blog.db") as connection:
+
+    # get a cursor object used to execute SQL commands
+    c = connection.cursor()
+
+    # create the table
+    c.execute("CREATE TABLE IF NOT EXISTS posts (title TEXT, post TEXT)")
+
+    # insert dummy values into table
+    c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
+    c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
+    c.execute('INSERT INTO posts VALUES("Excellent", "I\'m excellent.")')
+    c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
