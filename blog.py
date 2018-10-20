10d52a83c0a4285eb99d6ef30ef64c95cfd61cb7
David <david_kav@hotmail.com>
2018-10-20 15:33:48 +0200

    login/logout added.Session added. Setup template.html and the child html files

--- blog.py
+++ blog.py
@@ -2,10 +2,15 @@
 
 from flask import Flask, render_template, request, session, flash, redirect, url_for, g
 import sqlite3
+from functools import wraps
 
 app = Flask(__name__)
 
 DATABASE = 'blog.db'
+
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
 
 # pulls in app configuration by looking for UPPERCASE variables
 app.config.from_object(__name__)
@@ -13,5 +18,39 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to login first')
+            return redirect(url_for('login'))
+    return wrap
+
+@app.route("/", methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+            request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please Try Again'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
+
+@app.route("/main")
+@login_required
+def main():
+    return render_template('main.html')
+
+@app.route("/logout")
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
+
+
 if __name__ == "__main__":
-    app.run(DEBUG=True)
+    app.run()
--- sql.py
+++ sql.py
@@ -12,4 +12,4 @@ with sqlite3.connect("blog.db") as connection:
     c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
     c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
     c.execute('INSERT INTO posts VALUES("Excellent", "I\'m excellent.")')
-    c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
+    c.execute('INSERT INTO posts VALUES("Okay", "I\'m Okay.")')
